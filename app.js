4905b78d46ba4f6b0d37beee35371bb227bc20d2
seojin7022 <seojin7022@gmail.com>
2023-11-15 08:06:37 +0900

    Commit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const year = today.getFullYear().toString();
 const month = (today.getMonth() + 1).toString();
 const date = today.getDate().toString();
 
+const cur_meal = 0;
+
 fetch(
   `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=5bf6d0aa4370464793f2ed55b2269040&Type=json&ATPT_OFCDC_SC_CODE=R10&SD_SCHUL_CODE=8750161&MLSV_YMD=${
     year + month + date
@@ -19,14 +21,17 @@ fetch(
       meal.innerHTML = "등록된 식단이 없습니다.";
     } else {
       for (let i = 0; i < data.mealServiceDietInfo[1].row.length; i++) {
-        const newData = data.mealServiceDietInfo[1].row[i].DDISH_NM;
-        for (let j = 0; j < newData.split("<br/>").length; j++) {
-          const newMeal = document.createElement("li");
-          newMeal.innerHTML = newData.split("<br/>")[j];
-          meal.append(newMeal);
+        if (i == cur_meal) {
+          meal_title.innerHTML = i == 0 ? "중식" : "석식";
+
+          const newData = data.mealServiceDietInfo[1].row[i].DDISH_NM;
+          for (let j = 0; j < newData.split("<br/>").length; j++) {
+            const newMeal = document.createElement("li");
+            newMeal.innerHTML = newData.split("<br/>")[j];
+            newMeal.classList.add("fs-4", "list-group-item", "bg-transparent");
+            meal.append(newMeal);
+          }
         }
-        const line = document.createElement("hr");
-        meal.append(line);
       }
     }
   });
