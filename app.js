070a20b98f68b5498c0f7240e14950428e60fe6e
seojin7022 <seojin7022@gmail.com>
2024-04-07 00:50:45 +0900

    c

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
 const meal_title = container.querySelector("#meal-title");
+const breakfast = container.querySelector("#breakfast");
 const lunch = container.querySelector("#lunch");
 const dinner = container.querySelector("#dinner");
 const next_button = container.querySelector("#next-button");
@@ -12,29 +13,27 @@ const today = new Date();
 
 let cur_meal = 0;
 
+const createNewMeal = () => {
+  const newMeal = document.createElement("li");
+  newMeal.innerHTML = "등록된 식단이 없습니다.";
+  newMeal.classList.add(
+    "fs-4",
+    "list-group-item",
+    "bg-transparent",
+    "text-center"
+  );
+
+  return newMeal;
+}
+
 const refreshMeal = () => {
+  breakfast.innerHTML = "";
   lunch.innerHTML = "";
   dinner.innerHTML = "";
 
-  let newMeal = document.createElement("li");
-  newMeal.innerHTML = "등록된 식단이 없습니다.";
-  newMeal.classList.add(
-    "fs-4",
-    "list-group-item",
-    "bg-transparent",
-    "text-center"
-  );
-
-  lunch.append(newMeal);
-  newMeal = document.createElement("li");
-  newMeal.innerHTML = "등록된 식단이 없습니다.";
-  newMeal.classList.add(
-    "fs-4",
-    "list-group-item",
-    "bg-transparent",
-    "text-center"
-  );
-  dinner.append(newMeal);
+  breakfast.append(createNewMeal());
+  lunch.append(createNewMeal());
+  dinner.append(createNewMeal());
 
   const year = today.getFullYear().toString();
   const month = (today.getMonth() + 1).toString().padStart(2, '0');
@@ -53,6 +52,11 @@ const refreshMeal = () => {
       } else {
         for (let i = 0; i < data.mealServiceDietInfo[1].row.length; i++) {
           if (i == 0) {
+            const len = breakfast.children.length;
+            for (let j = 0; j < len; j++) {
+              breakfast.removeChild(breakfast.children[0]);
+            }
+          } else if (i == 1) {
             const len = lunch.children.length;
             for (let j = 0; j < len; j++) {
               lunch.removeChild(lunch.children[0]);
@@ -74,7 +78,8 @@ const refreshMeal = () => {
               "bg-transparent",
               "text-center"
             );
-            if (i == 0) lunch.append(newMeal);
+            if (i == 0) breakfast.append(newMeal);
+            else if (i == 1) lunch.append(newMeal);
             else dinner.append(newMeal);
           }
         }
@@ -96,14 +101,18 @@ refreshMeal();
 
 prevDay.addEventListener("click", () => {
   today.setDate(today.getDate() - 1);
+  breakfast.innerHTML = "";
   lunch.innerHTML = "";
+  dinner.innerHTML = "";
   updateDate();
   refreshMeal();
 });
 
 nextDay.addEventListener("click", () => {
   today.setDate(today.getDate() + 1);
+  breakfast.innerHTML = "";
   lunch.innerHTML = "";
+  dinner.innerHTML = "";
   updateDate();
   refreshMeal();
 });
